0a2d6244c4524cdc296721df4c5c5dc7c22dc72a
Yash Agarwal <yashagarwal7088@gmail.com>
2026-03-18 07:20:21 +0530

    show route done

--- app.js
+++ app.js
@@ -20,7 +20,10 @@ async function main(){
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended:true}));
+
 let port=8080;
+
 app.listen(port,()=>{
     console.log(`Server is listening on port ${port}`);
 })
@@ -29,6 +32,8 @@ app.get("/",(req,res)=>{
     res.send("hii i am get api");
 })
 
+
+//index route
 app.get("/listings",async (req,res)=>{
     //.find will return a promise, now to send it to ejs file we 
     //will make the function async coz it prevent callback hell(by mutliple then catch) in case of many things
@@ -40,6 +45,12 @@ app.get("/listings",async (req,res)=>{
     res.render("listings/index",{allListings});
 });
 
+//show route
+app.get("/listings/:id", async (req,res)=>{
+    let {id}=req.params;
+    const listing=await Listing.findById(id);
+    res.render("listings/show",{listing});
+})
 
 // app.get("/testListing", async (req,res)=>{
 //     let sampleListing= new Listing({
