468ab5248d391c5970b6c32375476f817b9393bc
Yash Agarwal <yashagarwal7088@gmail.com>
2026-04-07 10:55:05 +0530

    feat: make error.ejs

--- app.js
+++ app.js
@@ -128,7 +128,8 @@ app.use((req, res, next) => {
 //custom error handler-> this is the actual handler
 app.use((err, req, res, next) => {
   let { statusCode = 500, message = "Something went wrong" } = err;
-  res.status(statusCode).send(message);
+//   res.status(statusCode).send(message);
+    res.render('error.ejs',{err});
 });
 
 let port = 8080;
