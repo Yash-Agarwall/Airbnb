2b2fb08b66f00878f3b81c722242acf400a5c201
Yash Agarwal <yashagarwal7088@gmail.com>
2026-04-19 16:04:02 +0530

    refactor: user router.route for user routes, imrove readability

--- routes/user.js
+++ routes/user.js
@@ -6,23 +6,24 @@ const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
 
 const userController = require("../controllers/user.js");
-router.get("/signup", userController.renderSignupForm);
-
-router.post("/signup", userController.signup);
+router
+  .route("/signup")
+  .get(userController.renderSignupForm)
+  .post(userController.signup);
 
 //login routes
-router.get("/login", userController.renderLoginForm);
-
-router.post(
-  "/login",
-  saveRedirectUrl,
-  passport.authenticate("local", {
-    failureRedirect: "/login",
-    failureFlash: true,
-  }),
-  userController.login,
-);
+router
+  .route("/login")
+  .get(userController.renderLoginForm)
+  .post(
+    saveRedirectUrl,
+    passport.authenticate("local", {
+      failureRedirect: "/login",
+      failureFlash: true,
+    }),
+    userController.login,
+  );
 
 //logout routes
-router.get("/logout", userController.logout);
+router.route("/logout").get(userController.logout);
 module.exports = router;
