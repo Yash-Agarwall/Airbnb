a655a1dbddb25873f1e4bfe73d8d48e63f56d666
Yash Agarwal <yashagarwal7088@gmail.com>
2026-04-19 11:10:33 +0530

    refactor: middleware refactor

--- routes/listing.js
+++ routes/listing.js
@@ -5,16 +5,8 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const { listingSchema } = require("../schema.js");
 const Listing = require("../models/listing.js");
 const ExpressError = require("../utils/ExpressError.js");
-const { isLoggedIn, isOwner } = require("../middleware.js");
-const validateListing = (req, res, next) => {
-  let { error } = listingSchema.validate(req.body);
-  if (error) {
-    let errMsg = error.details.map((el) => el.message).join(", ");
-    throw new ExpressError(400, errMsg);
-  } else {
-    next();
-  }
-};
+const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
+
 
 router.param("id", (req, res, next, id) => {
   if (!mongoose.Types.ObjectId.isValid(id)) {
